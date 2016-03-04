354cbd8339f1eabff8b2781eee60463c20de9e49
leokhachatorians <leokhachatorians@gmail.com>
2016-03-03 22:48:02 -0800

    Figuring out note transposing

--- chapter_0/note_transposing/src/main.rs
+++ chapter_0/note_transposing/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let table: ['&static str; 12] = ["C", "C#", "D", "D#",
+                                     "E", "F", "F#", "G",
+                                     "G#", "A", "A#", "B"];
+    let mut interval = 0;
+}
+
+int mod12(interval: i32)
+{
+    while interval < 0
+    {
+        interval += 12;
+    }
+    while interval >= 12 
+    {
+        interval -= 12;
+    }
+    interval
 }
