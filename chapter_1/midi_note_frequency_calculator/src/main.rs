c6990db196fee70d9a8563167b7c536b70791503
leokhachatorians <leokhachatorians@gmail.com>
2016-03-05 00:54:58 -0800

    Added a Macro

--- chapter_1/midi_note_frequency_calculator/src/main.rs
+++ chapter_1/midi_note_frequency_calculator/src/main.rs
@@ -1,13 +1,44 @@
 use std::io;
+use std::env;
+use std::io::Write;
 
+macro_rules! error {
+    ($code: expr, $fmt:expr) => {{
+        use std::io::Write;
+        let err = std::io::stderr();
+        let _ = writeln!(&mut err.lock(), $fmt);
+        std::process::exit($code);
+    }};
+    ($code: expr, $fmt:expr, $($arg:tt)*) => {{
+        use std::io::Write;
+        let err = std::io::stderr();
+        let _ = writeln!(&mut err.lock(), $fmt, $($arg)*);
+        std::process::exit($code);
+    }};
+}
 
 fn main() {
     let semitone_ratio = 2.0_f64.powf(1.0/12.0);
     
     let c5 = 220.0 * semitone_ratio.powf(3.0);
     let c0 = c5 * 0.5_f64.powf(5.0);
+    
+    let args: Vec<_> = env::args().collect();
+    let mut midinote = 0;
 
-    let midinote = get_midinote();
+    if args.len() > 1 {
+        let arg_midinote = &mut midinote;
+
+        *arg_midinote = match args[1].parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => error!(1, "Error: Argument was not an integer")
+        };
+    }
+    else {
+        let no_arg_midinote = &mut midinote;
+        *no_arg_midinote = get_midinote();
+    }
+
     if midinote > 127 {
         println!("{} is above the MIDI range, try again.", midinote);
     }
